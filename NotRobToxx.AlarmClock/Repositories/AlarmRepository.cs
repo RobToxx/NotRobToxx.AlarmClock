42e876305c12b904bdc78c479df66b7da00bd5ca
NotRobToxx <ortizh0705@gmail.com>
2026-06-04 00:12:03 -0600

    feat(repositories): add AlarmRepository for managing alarm data
    Introduce thread-safe AlarmRepository to store, retrieve, update, and
    remove alarms using a Hashtable. This enhances data persistence and
    management within the AlarmClock application.
    
    Adds dependency on `nanoFramework.System.Collections`.

---
+++ NotRobToxx.AlarmClock/Repositories/AlarmRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using NotRobToxx.AlarmClock.Models;
+
+namespace NotRobToxx.AlarmClock.Repositories {
+    
+    public class AlarmRepository {
+        
+        private readonly Hashtable alarms = new();
+        
+        public AlarmRepository() {
+            
+        }
+        
+        private readonly object syncRoot = new();
+
+        public Alarm[] GetAll() {
+
+            lock (this.syncRoot) {
+                
+                var all = new Alarm[this.alarms.Count];
+                
+                this.alarms.Values.CopyTo(all, 0);
+                
+                return all;
+            }
+        }
+
+        public bool Add(Alarm alarm) {
+
+            lock (this.syncRoot) {
+
+                if (this.alarms.Contains(alarm.Id)) return false;
+
+                this.alarms.Add(alarm.Id, alarm);
+                return true;
+            }
+        }
+
+        public bool Remove(Guid id) {
+
+            lock (this.syncRoot) {
+
+                if (!this.alarms.Contains(id)) return false;
+
+                this.alarms.Remove(id);
+                return true;
+
+            }
+        }
+
+        public bool Update(Alarm alarm) {
+
+            lock (this.syncRoot) {
+
+                if (!this.alarms.Contains(alarm.Id)) return false;
+
+                this.alarms[alarm.Id] = alarm;
+                return true;
+
+            }
+        }
+    }
+}
