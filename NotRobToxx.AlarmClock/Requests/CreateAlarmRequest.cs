fa1ad32064bd269143c4b5b491859e190f4999f7
NotRobToxx <ortizh0705@gmail.com>
2026-07-18 10:10:01 -0600

    feat(services): add CreateAlarm functionality in AlarmService
    Implement Create method to validate and create alarms with schedule,
    snooze, and name validation. Introduce `CreateAlarmRequest` struct
    for cleaner input handling and integrate with AlarmRepository for
    persistence.

---
+++ NotRobToxx.AlarmClock/Requests/CreateAlarmRequest.cs
@@ -0,0 +1,10 @@
+﻿using NotRobToxx.AlarmClock.Models;
+
+namespace NotRobToxx.AlarmClock.Requests {
+    
+    public struct CreateAlarmRequest {
+        public string Name { get; set; }
+        public Schedule Schedule { get; set; }
+        public Snooze Snooze { get; set; }
+    }
+}
--- NotRobToxx.AlarmClock/Services/AlarmService.cs
+++ NotRobToxx.AlarmClock/Services/AlarmService.cs
@@ -1,6 +1,63 @@
-﻿namespace NotRobToxx.AlarmClock.Services {
+﻿using System;
+using NotRobToxx.AlarmClock.Models;
+using NotRobToxx.AlarmClock.Repositories;
+using NotRobToxx.AlarmClock.Requests;
+
+namespace NotRobToxx.AlarmClock.Services {
     
     public class AlarmService {
+
+        private readonly AlarmRepository alarmRepository;
         
+        public AlarmService(AlarmRepository alarmRepository) {
+            
+            this.alarmRepository = alarmRepository;
+        }
+
+        public void Create(CreateAlarmRequest request) {
+
+            if (request.Name.Length == 0) {
+                
+                throw new ArgumentException(
+                    "Alarm name cannot be empty.", nameof(request.Name)
+                );
+            }
+            
+            if (!request.Snooze.IsEmpty) {
+
+                if (request.Snooze.Count > 10) {
+                    
+                    throw new ArgumentException(
+                        "Snooze count cannot be greater than 10.", nameof(request.Snooze)
+                    );
+                }
+                
+                if (request.Snooze.Interval > 30) {
+                    
+                    throw new ArgumentException(
+                        "Snooze interval cannot be greater than 30 minutes.", nameof(request.Snooze)
+                    );
+                }
+            }
+            
+            var valid = request.Schedule.TryGetNextOccurrence(DateTime.UtcNow, out var _);
+
+            if (!valid) {
+
+                throw new ArgumentException(
+                    "Invalid schedule.", nameof(request.Schedule)
+                );
+            }
+
+            var alarm = new Alarm {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Schedule = request.Schedule,
+                Snooze = request.Snooze,
+                Enabled = true
+            };
+            
+            this.alarmRepository.Add(alarm);
+        }
     }
 }
