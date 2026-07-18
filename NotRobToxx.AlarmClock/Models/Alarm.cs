c4873464abe40e16126a0d7169dae7f1b3ecbf27
NotRobToxx <ortizh0705@gmail.com>
2026-07-18 10:09:41 -0600

    feat(models): add Snooze model and integrate with Alarm
    Introduce `Snooze` struct to encapsulate snooze interval and count logic,
    replacing `RepeatCount` and `RepeatInterval` in `Alarm` for better clarity
    and maintainability.

--- NotRobToxx.AlarmClock/Models/Alarm.cs
+++ NotRobToxx.AlarmClock/Models/Alarm.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
 namespace NotRobToxx.AlarmClock.Models {
-
     public struct Alarm {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public bool Enabled { get; set; }
         public Schedule Schedule { get; set; }
-        public byte RepeatCount { get; set; }
-        public TimeSpan RepeatInterval { get; set; }
+        public Snooze Snooze { get; set; }
     }
 }
---
+++ NotRobToxx.AlarmClock/Models/Snooze.cs
@@ -0,0 +1,9 @@
+﻿namespace NotRobToxx.AlarmClock.Models {
+    
+    public struct Snooze {
+        public byte Interval { get; set; }
+        public byte Count { get; set; }
+        
+        public bool IsEmpty => this.Interval == 0 && this.Count == 0;
+    }
+}
